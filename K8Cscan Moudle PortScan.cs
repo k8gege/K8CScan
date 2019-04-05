7d51d266aaf4aa320a10cda16fe177eb7d0df1fa
k8gege <42312878+k8gege@users.noreply.github.com>
2019-04-05 23:39:59 +0800

    Add files via upload

--- K8Cscan Moudle PortScan.cs
+++ K8Cscan Moudle PortScan.cs
@@ -4,7 +4,7 @@ using System.Text;
 using System.Net;
 using System.Text.RegularExpressions;
 using System.Net.Sockets;
-//Cscan 3.1 PortScan Moudle Demo
+//Cscan 3.1 PortScan Moudle
 namespace CscanDLL
 {
     public class scan
@@ -16,17 +16,17 @@ namespace CscanDLL
             else
             {
                 if (K8CheckPort(ip, 21))
-                    return ip + "\t21 Open\r\n";
+                    Console.Write(ip + "\t21 Open\r\n");
                 if (K8CheckPort(ip, 80))
-                    return ip + "\t80 Open\r\n";
+                    Console.Write(ip + "\t80 Open\r\n");
                 if (K8CheckPort(ip, 1433))
-                    return ip + "\t1433 Open\r\n";
+                    Console.Write(ip + "\t1433 Open\r\n");
                 if (K8CheckPort(ip, 3306))
-                    return ip + "\t3306 Open\r\n";
+                    Console.Write(ip + "\t3306 Open\r\n");
                 if (K8CheckPort(ip, 1521))
-                    return ip + "\t1521 Open\r\n";
+                    Console.Write(ip + "\t1521 Open\r\n");
                 if (K8CheckPort(ip, 3389))
-                    return ip + "\t3389 Open\r\n";
+                    Console.Write(ip + "\t3389 Open\r\n");
             }
 
             return "";
